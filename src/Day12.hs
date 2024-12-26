5b88220738b38e962a5dade3eed504e30f208b3b
pressxtohonk <pressxtohonk@gmail.com>
2024-12-26 23:09:24 +0800

    Day 12: part 2

--- src/Day12.hs
+++ src/Day12.hs
@@ -1,45 +1,47 @@
 module Main where
 
 import Data.List (intercalate, repeat, replicate)
+import Data.MemoTrie (memo2)
 import PressXToParse
 import PressXToSolve (Solver, runCLI)
-import Text.Parsec (char, choice, manyTill, sepBy, space, try)
+import Text.Parsec (char, manyTill, sepBy, space, try, oneOf)
 
-data Condition = O | D | U deriving (Eq, Show)
+conditionP :: Parser Char
+conditionP = oneOf ".#?"
 
-conditionP :: Parser Condition
-conditionP =
-  choice . map try $
-    [ O <$ char '.',
-      D <$ char '#',
-      U <$ char '?'
-    ]
-
-springP :: Parser ([Condition], [Int])
+springP :: Parser (String, [Int])
 springP = (,) <$> conditionP `manyTill` space <*> int `sepBy` char ','
 
-numMatches :: [Condition] -> [Int] -> Int
-numMatches [] [] = 1
-numMatches [] ns = 0
-numMatches (O : t) [] = numMatches t []
-numMatches (D : _) [] = 0
-numMatches (U : t) [] = numMatches t []
-numMatches xs@(h : t) (n : ns) = accept + reject
+numMatches :: String -> [Int] -> Int
+numMatches = memo2 go
   where
-    accept = if h /= O && matches xs n then numMatches (drop (n + 1) xs) ns else 0
-    reject = if h /= D then numMatches t (n : ns) else 0
+    go [] [] = 1
+    go [] ns = 0
+    go ('.' : t) [] = numMatches t []
+    go ('#' : _) [] = 0
+    go ('?' : t) [] = numMatches t []
+    go xs@(h : t) (n : ns) = accept + reject
+      where
+        accept = if h /= '.' && matches xs n then numMatches (drop (n + 1) xs) ns else 0
+        reject = if h /= '#' then numMatches t (n : ns) else 0
 
-matches :: [Condition] -> Int -> Bool
+matches :: String -> Int -> Bool
 matches xs n
   | length xs < n = False
-  | length xs > n = O `notElem` take n xs && xs !! n /= D
-  | otherwise = O `notElem` xs
+  | length xs > n = '.' `notElem` take n xs && xs !! n /= '#'
+  | otherwise = '.' `notElem` xs
+
+unfold :: Int -> (String, [Int]) -> (String, [Int])
+unfold n (xs, ns) = (fx xs, fn ns)
+  where
+    fx = intercalate "?" . replicate n
+    fn = concat . replicate n
 
 solve1 :: Solver
 solve1 = show . sum . map (uncurry numMatches) . mustParse (linesOf springP)
 
 solve2 :: Solver
-solve2 = show
+solve2 = show. sum . map (uncurry numMatches . unfold 5) . mustParse (linesOf springP)
 
 main :: IO ()
 main = runCLI solve1 solve2
